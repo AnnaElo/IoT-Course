0a5914ed26a712c3d07d12f7b727250a1f7fb8aa
Jessica <suarez.jessica1992@hotmail.com>
2024-02-07 15:56:42 +0200

    starting point for 2_2_4

--- exercise2_2_4/main.py
+++ exercise2_2_4/main.py
@@ -12,9 +12,35 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
+# Initialize
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+
 # Create your objects here.
 ev3 = EV3Brick()
 
+# Change of Direction and Speed.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=54, axle_track=105)
 
-# Write your program here.
-ev3.speaker.beep()
+# Initial speed in mm/s
+initial_speed = 100
+# Speed increment in mm/s (how much to increase speed each second)
+speed_increment = 50
+# Current speed
+current_speed = initial_speed
+
+# Duration of acceleration in seconds
+acceleration_duration = 10
+
+# Start driving forward and accelerate each second
+for _ in range(acceleration_duration):
+    # Set the robot to drive at the current speed
+    robot.drive(current_speed, 0)
+    # Wait for 1 second
+    time.sleep(1)
+    # Increase the speed for the next iteration
+    current_speed += speed_increment
+
+# After finishing the acceleration, stop the robot
+robot.stop()
