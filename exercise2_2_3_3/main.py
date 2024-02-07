ad5fb084b581bfd193bfa0ab074845916697afc4
Jessica <suarez.jessica1992@hotmail.com>
2024-02-07 15:48:40 +0200

    exercise 2_2_3_3 DONE

--- exercise2_2_3_3/main.py
+++ exercise2_2_3_3/main.py
@@ -21,9 +21,13 @@ ev3 = EV3Brick()
 
 # Change of Direction and Speed.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=54, axle_track=105)
-robot.drive(600, 0) 
+
+robot.settings(straight_speed=1000)
+
+
+robot.straight(1000) 
 time.sleep(2)       
 ev3.speaker.beep()
-robot.drive(-300, 0) 
-time.sleep(2)
+robot.straight(-1000)  
+robot.stop()
 
