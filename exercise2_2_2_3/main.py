2d79ab830e82eb65efb7fce96efe9cf38261313c
Your Name <you@example.com>
2024-02-08 10:13:49 +0200

    Fixing infinite loop in the first exercise & making code structure similar.

--- exercise2_2_2_3/main.py
+++ exercise2_2_2_3/main.py
@@ -7,17 +7,12 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
 import time
 
-# Initialize the motors.
+# Initializing.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-# Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 # Create your objects here.
