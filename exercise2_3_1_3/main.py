7a06f62943b177b0e82097ba9ec87a4e1a0f8083
Jessica <suarez.jessica1992@hotmail.com>
2024-04-02 02:14:37 +0300

    exercise 2313 TO CHECK

--- exercise2_3_1_3/main.py
+++ exercise2_3_1_3/main.py
@@ -15,6 +15,26 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
+# Initialize.
+TSensor = TouchSensor(Port.S1)
 
-# Write your program here.
-ev3.speaker.beep()
+# Count how often the touch sensor has been pressed
+# When the program starts, the robot shall count for 5 seconds, how often the touch sensor has been pressed. 
+# This number shall be shown on the display.
+# After the 5 seconds, the robot shall play a tone as many times as the touch sensor was pressed. 
+
+# Code structure
+
+button_count = 0
+watch=StopWatch()
+
+while watch.time() < 5000: 
+    if TSensor.pressed():
+        button_count += 1
+        ev3.screen.clear()
+        print("Count: {}" .format(button_count)) #cal also be ev3.screen.print()
+    wait(10) #10 milisecond wait
+
+#for the sound part
+for i in range(button_count):
+    ev3.speaker.beep()
