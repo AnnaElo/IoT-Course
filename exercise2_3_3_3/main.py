276f2465a9d722fb5d7b70792b1d976d64ad6cee
Jessica <suarez.jessica1992@hotmail.com>
2024-04-02 02:52:20 +0300

    exercise 2333 TO CHECK

--- exercise2_3_3_3/main.py
+++ exercise2_3_3_3/main.py
@@ -8,13 +8,34 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
 # Create your objects here.
 ev3 = EV3Brick()
 
+# Initialize. 
+CSensor = ColorSensor(Port.S3)
+ev3.screen.print(CSensor.color())
 
-# Write your program here.
-ev3.speaker.beep()
+# The robot should play a different sound depending on the color when recognizing different colors.
+
+while True:
+    # Get the color detected by the sensor.
+    color = CSensor.color()
+    
+    # Compare the detected color with predefined color values.
+    if color == Color.RED:
+        ev3.speaker.beep(frequency=261.63, duration=500)  # C note
+    elif color == Color.GREEN:
+        ev3.speaker.beep(frequency=293.66, duration=500)  # D note
+    elif color == Color.BLUE:
+        ev3.speaker.beep(frequency=329.63, duration=500)  # E note
+    elif color == Color.YELLOW:
+        ev3.speaker.beep(frequency=349.23, duration=500)  # F note
+    elif color == Color.WHITE:
+        ev3.speaker.beep(frequency=392.00, duration=500)  # G note
+    elif color == Color.BLACK:
+        ev3.speaker.beep(frequency=440.00, duration=500)  # A note
+    else:
+        ev3.speaker.beep(frequency=0, duration=500)  # No beep if not detected color
+
+    # Add a short delay to avoid excessive printing.
+    wait(100)
