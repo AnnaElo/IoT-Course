96dae28e8385869b3fdc447e95723f245981eca2
Yogi Nisha <yogi@nisha.com>
2024-04-15 02:42:16 -0700

    2343

--- exercise2_3_4_3/main.py
+++ exercise2_3_4_3/main.py
@@ -6,33 +6,61 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from umqtt.simple import MQTTClient
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+MQTT_BROKER = "your_broker_ip"
+MQTT_TOPIC = "robot_commands"
 
 
-# Create your objects here.
+def play_tone(button):
+    if button == Button.UP:
+        ev3.speaker.beep(1000, 500)
+    elif button == Button.DOWN:
+        ev3.speaker.beep(1500, 500)
+    elif button == Button.LEFT:
+        ev3.speaker.beep(2000, 500)
+    elif button == Button.RIGHT:
+        ev3.speaker.beep(2500, 500)
+    elif button == Button.CENTER:
+        ev3.speaker.beep(3000, 500)
+
+
+def mqtt_callback(topic, msg):
+    button = Button[msg.decode()]
+    play_tone(button)
+
+
+client = MQTTClient("ev3", MQTT_BROKER)
+client.set_callback(mqtt_callback)
+client.connect()
+
+
+client.subscribe(MQTT_TOPIC)
+
+
 ev3 = EV3Brick()
 
 
-# Create your objects here.
-ev3 = EV3Brick()
-
-
-# Write your program here // Only one button pressed at the time
 while True:
+    client.check_msg()  
     pressed_buttons = ev3.buttons.pressed()
     ev3.screen.clear()
 
     if Button.UP in pressed_buttons:
         ev3.screen.print("Up button pressed")
+        client.publish(MQTT_TOPIC, "UP")
     elif Button.DOWN in pressed_buttons:
         ev3.screen.print("Down button pressed")
+        client.publish(MQTT_TOPIC, "DOWN")
     elif Button.LEFT in pressed_buttons:
         ev3.screen.print("Left button pressed")
+        client.publish(MQTT_TOPIC, "LEFT")
     elif Button.RIGHT in pressed_buttons:
         ev3.screen.print("Right button pressed")
+        client.publish(MQTT_TOPIC, "RIGHT")
     elif Button.CENTER in pressed_buttons:
-        ev3.screen.print("Center button pressed")                      
+        ev3.screen.print("Center button pressed")
+        client.publish(MQTT_TOPIC, "CENTER")
+        
     wait(100)
